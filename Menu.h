201ab8dbc622022ab67d155b6bba4557849b14a0
XiaaakL <24-0747@student.gutech.edu.om>
2026-05-09 12:38:23 +0400

    Create Menu.h

---
+++ Menu.h
@@ -0,0 +1,15 @@
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+
+// Menu class (gonna compose from Resturant)
+class Menu{
+    private:
+        vector<string> itemNames; //vector of the item name
+        vector<double> itemPrices;//vector of the item price
+    public:
+        Menu(); //we start with an empty menu
+        void addItem(string n, double p);
+    	void display() const; //reads only (we use const here)
+};
