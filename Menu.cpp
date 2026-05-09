e819c1d803c5cf34b87a52833b032858d1a76180
XiaaakL <24-0747@student.gutech.edu.om>
2026-05-09 12:42:53 +0400

    Create Menu.cpp

---
+++ Menu.cpp
@@ -0,0 +1,13 @@
+#include "Menu.h"
+#include <iostream>
+using namespace std;
+
+Menu::Menu(){}
+void Menu::addItem(string n, double p){
+        itemNames.push_back(n);//adds the name  to the end of the names list (the point of using push_back)
+        itemPrices.push_back(p); //adds the pricr to the end of the prices list
+}
+void Menu::display() const{
+    for(int i=0;i<itemNames.size(); i++) //from the first to the last element in the name vector (the menu)
+        cout<<" - "<< itemNames[i]<<" : "<<itemPrices[i]<<" OMR"<<endl;
+}
