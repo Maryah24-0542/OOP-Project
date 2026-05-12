5c7230c63ee175cd85395137c59e48d4737e077c
Maryah24-0542 <24-0542@student.gutech.edu.om>
2026-05-12 20:54:51 +0400

    Update Restaurant.cpp

--- Restaurant.cpp
+++ Restaurant.cpp
@@ -6,7 +6,7 @@ Restaurant::Restaurant(string n,string co, string adr):name(n),cuisine(co),addre
   		stringstream ss;
         ss<<"R-"<<setfill('0')<<setw(4)<<count++; //total of 4 digits (including the number from count)...  i guess
         restaurantId=ss.str(); //turn it into a string like "R-0001"
-        cout<<"Resturant: "<<name<<"has been added, your resturant ID: "<<restaurantId<<endl;
+        cout<<"Restaurant: "<<name<<"has been added, your restaurant ID: "<<restaurantId<<endl;
 }
 
 void Restaurant::addToMenu(string itemName, double price) {
