6ae1b9b9a6fb000db5ca930501cdb77124f8957e
Lama <24-0747@student.gutech.edu.om>
2026-05-12 17:11:05 +0400

    Update Restaurant.cpp

--- Restaurant.cpp
+++ Restaurant.cpp
@@ -2,13 +2,22 @@
 #include <iostream>
 using namespace std;
 
-Restaurant::Restaurant(int id, string n):restaurantId(id),name(n) {}
-void Restaurant::addToMenu(string itemName, double price){
+Restaurant::Restaurant(string n,string co, string adr):name(n),cuisine(co),address(adr) {
+  		stringstream ss;
+        ss<<"R-"<<setfill('0')<<setw(4)<<count++; //total of 4 digits (including the number from count)...  i guess
+        restaurantId=ss.str(); //turn it into a string like "R-0001"
+        cout<<"Resturant: "<<name<<"has been added, your resturant ID: "<<restaurantId<<endl;
+}
+
+void Restaurant::addToMenu(string itemName, double price) {
   menu.addItem(itemName, price);
-  }
+}
 void Restaurant::display() const{
         cout<<"======================================="<<endl;
         cout<<"Restaurant : "<<name<<endl;
+        cout<<"Cusinine : "<<cuisine<<endl;
+        cout<<"Restaurant ID: "<<restaurantId<<endl;
+        cout<<"location : "<<address<<endl;
         cout<<"Menu: "<<endl;
         menu.display();
         cout<<"======================================="<<endl;
