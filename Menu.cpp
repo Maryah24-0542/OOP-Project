e107c7472436e23903aa4c88782f136818b68fb7
Alghala Alkalbani <24-0309@student.gutech.edu.om>
2026-05-12 23:28:15 +0400

    Update Menu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -6,7 +6,20 @@ Menu::Menu(){}
 void Menu::addItem(string n, double p){
         itemNames.push_back(n);//adds the name  to the end of the names list (the point of using push_back)
         itemPrices.push_back(p); //adds the pricr to the end of the prices list
+        
 }
+ //  Validate item exists in Restaurant menu
+   bool Menu::hasItem(string name) const {
+          for (int i = 0; i < itemNames.size(); i++)
+           if (itemNames[i] == name) return true;
+          return false;
+      }
+    
+      double Menu::getItemPrice(string name) const {
+        for (int i = 0; i < itemNames.size(); i++)
+             if (itemNames[i] == name) return itemPrices[i];
+        return -1;
+      }
 void Menu::display() const{
     for(int i=0;i<itemNames.size(); i++) //from the first to the last element in the name vector (the menu)
         cout<<" - "<< itemNames[i]<<" : "<<itemPrices[i]<<" OMR"<<endl;
