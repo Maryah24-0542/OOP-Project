2325021d3fd3ac05aee99daee7225b1423f97405
Maryah24-0542 <24-0542@student.gutech.edu.om>
2026-05-12 02:39:00 -0400

    Add files via upload

---
+++ customer.h
@@ -0,0 +1,32 @@
+#ifndef UNTITLED_CUSTOMER_H
+#define UNTITLED_CUSTOMER_H
+#endif //UNTITLED_CUSTOMER_H
+
+# include<iostream>
+#include <vector>
+using namespace std;
+
+class Order;
+class Customer {
+private:
+    static int NextID;
+    int PhoneNumber,CustomerID;
+    string Name,Email,Address;
+    vector<Order*> orderHistory;
+public:
+    Customer();
+    Customer( int phone, string name,string email,string address);
+    void PLaceOrder(Order* order);
+    void setPhone();
+    void setName();
+    void setEmail();
+    void setAddress();
+    int getPhone();
+    string getName();
+    string getEmail();
+    string getAddress();
+    int getCustomerID();
+    vector<Order*> getOrderHistory();
+    void Display();
+
+};
