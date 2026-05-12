c8c9d3815289d1a5494f3811a3fa3cae047433d5
Lama Al-Muharbi <24-0747@student.gutech.edu.om>
2026-05-12 23:10:36 +0400

    Update Restaurant.h

--- Restaurant.h
+++ Restaurant.h
@@ -7,16 +7,19 @@ using namespace std;
 
 class Restaurant{
 	private:
-    	string restaurantId;
+    	string restaurantId; //the output gonna be like "R-0001"
     	string name;
         string cuisine;
         string address;
-    	Menu menu; //Restaurant has-a Menu
+    	Menu menu; //Restaurant has-a Menu (compsition)
         static int count;
 
 	public:
     	Restaurant(string n, string co, string adr);
     	void addToMenu(string itemName,double price);
     	void display() const;
-
+        string getName() const;
+        string getCusine() const;
+        string getAddress() const;
+        string getId() const;
 };
