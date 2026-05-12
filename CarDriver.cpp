6a1c30402da56d579e827c39db15e4b4fd4943f1
Maryah24-0542 <24-0542@student.gutech.edu.om>
2026-05-12 03:11:37 -0400

    Add files via upload

---
+++ CarDriver.cpp
@@ -0,0 +1,48 @@
+#include "Driver.h"
+#include "CarDriver.h"
+#include<iostream>
+using namespace std;
+
+
+
+CarDriver::CarDriver() {
+
+}
+
+void CarDriver::setSeatingCapacity() {
+    cout<<"Enter seating capacity of car: ";
+    cin>>seatingCapacity;
+}
+
+int CarDriver::getSeatingCapacity() {
+    return seatingCapacity;
+}
+
+void CarDriver::setBaseFarePerKm() {
+    cout<<"Enter base fare per km :";
+    cin>>baseFarePerKm;
+}
+
+double CarDriver::getBaseFarePerKm() {
+    return baseFarePerKm;
+}
+
+void CarDriver::setCarModel() {
+    cout<<"Enter Car Model :";
+    cin>>carModel;
+}
+string CarDriver::getCarModel() {
+    return carModel;
+}
+
+void CarDriver::showDetails() {
+    cout<<"Car Model :"<<getCarModel()<<"\n";
+    cout<<"Seating capacity of the car : "<<getSeatingCapacity()<<"\n";
+    cout<<"Base fare per kilometer :"<<getBaseFarePerKm()<<"\n";
+    cout<<"Delivery fee is : "<<calcDeliveryFee();
+    cout<<"Estimated time for delivery : "<<getESTtime();
+
+
+
+
+}
