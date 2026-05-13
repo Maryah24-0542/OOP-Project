010dc182a7646ddf9ebb05b16a11df74fa9d578f
Maryah24-0542 <24-0542@student.gutech.edu.om>
2026-05-13 20:34:55 +0400

    Update CarDriver.cpp

--- CarDriver.cpp
+++ CarDriver.cpp
@@ -45,9 +45,9 @@ string CarDriver::getPlateNumber() {
 }
 
 void CarDriver::showDetails() {
-    cout<<"Car Model :"<<getCarModel()<<"\n";
-    cout<<"Seating capacity of the car : "<<getSeatingCapacity()<<"\n";
-    cout<<"Base fare per kilometer :"<<getBaseFarePerKm()<<"\n";
+    cout<<"Car Model -> "<<getCarModel()<<"\n";
+    cout<<"Seating capacity of the car -> "<<getSeatingCapacity()<<"\n";
+    cout<<"Base fare per kilometer -> "<<getBaseFarePerKm()<<"\n";
     cout<<"Plate number -> "<<getPlateNumber()<<"\n";
     cout<<"Delivery fee is -> "<<calcDeliveryFee()<<"\n";
     cout<<"Estimated time for delivery -> "<<getESTtime()<<"\n";
