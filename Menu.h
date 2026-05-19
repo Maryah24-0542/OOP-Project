47fa69c8968dff4e83969ac6de9cc14ff5ae0f29
Lama Al-Muharbi <24-0747@student.gutech.edu.om>
2026-05-19 19:21:22 +0400

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -14,6 +14,11 @@ class Menu{
     public:
         Menu(); //we start with an empty menu
         void addItem(string n, double p);
-        double getItemPrice(string name) const;
     	void display() const; //reads only (we use const here)
+		string getItemName(int ch) const;
+		double getItemPrice(int ch) const;
+		bool isItemValid(int ch) const;
+		//setters- in case we wanna update the item
+		void setItemName(int ch, string nwName);
+		void setItemPrice(int ch, double nwprice);
 };
