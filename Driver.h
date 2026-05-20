b531ac9278c88975d97891d2eefc8a013c6d5f56
Maryah24-0542 <24-0542@student.gutech.edu.om>
2026-05-20 10:30:56 +0400

    Update Driver.h

--- Driver.h
+++ Driver.h
@@ -1,14 +1,14 @@
 
-#ifndef UNTITLED_DRIVER_H
-#define UNTITLED_DRIVER_H
-
-#endif //UNTITLED_DRIVER_H
-
+#ifndef DRIVER_H
+#define DRIVER_H
 #include<iostream>
 #include<vector>
 #include<string>
+#include "order.h"
+
 
 using namespace std;
+
 
 class Driver {
 private:
@@ -21,30 +21,45 @@ private:
     double earnings;
 
 public:
+    Order ord;
+
     Driver();
-    Driver(int ID,string n,int p,string vn,bool isav,double e=0.0)
-    : DriverID(ID),DriverName(n),DriverPhoneNumber(p),vehicleNumber(vn),isAvailable(isav),earnings(e){}
-    virtual double calcDeliveryFee(double distance)=0;
-    virtual double getESTtime(double distance)=0;
+
+    Driver(int ID, string n, int p, string vn, bool isav, double e = 0.0)
+        : DriverID(ID), DriverName(n), DriverPhoneNumber(p), vehicleNumber(vn), isAvailable(isav), earnings(e) {
+    }
+
+    virtual double calcDeliveryFee(double distance) =0;
+
+    virtual double getESTtime(double distance) =0;
     virtual ~Driver();
 
     void setDriverID();
+
     void setName();
+
     void setNumber();
+
     void setVehicleNumber();
+
     void setEarnings();
 
+    void setAvailability(bool a) {
+        isAvailable = a;
+    }
+
     int getDriverID();
+
     string getName();
+
     int getNumber();
+
     string getVehicleNumber();
+
     double getEarnings();
 
     void showDetails();
 
     int assignOrder(int orderID);
-void setAvailability(bool a){
-    isAvailable = a;
-}
-
 };
+#endif
